ceac11a22142614a4c5fe51bd4c1251ad215c4c3
Paul Dredge <lardarse7@hotmail.com>
2023-02-17 20:43:29 +0000

    Added special attack functionality for every 3 rounds

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ const app = Vue.createApp({
     data() {
         return {
             playerHP: 100,
-            monsterHP: 100
+            monsterHP: 100,
+            currentTurn: 0
         }
     },
     methods: {
         playerAttack() {
             this.monsterHP -= calculateAttack(5, 12);
             this.monsterAttack();
+            this.currentTurn += 1;
         },
         monsterAttack() {
             this.playerHP -= calculateAttack(8, 15);
         },
+        specialAttack() {
+            this.monsterHP -= calculateAttack(10, 25);
+            this.monsterAttack();
+            this.currentTurn += 1;
+        }
     },
     computed: {
         playerBarWidth() {
@@ -22,6 +29,9 @@ const app = Vue.createApp({
         },
         monsterBarWidth() {
             return {width: this.monsterHP + '%'}
+        },
+        specialAttackAvailable() {
+            return this.currentTurn % 3 !== 0;
         }
     },
 });
